0036dddc4bde56dd337ba477c3ad789e30bbb646
BuddhikaD <buddhiextuse@gmail.com>
2021-01-19 15:45:44 +0100

    add data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,9 +8,23 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Trump', id: 3 },
   ]);
 
+  const [name, setName] = useState('John');
+
+  const handleDelete = (id) => {
+    const newBlog = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlog);
+  };
+
+  useEffect(() => {
+    console.log('user effect run');
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 };
