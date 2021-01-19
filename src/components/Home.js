db3874395f9c076d0bb242e59227fb40969e213c
BuddhikaD <buddhiextuse@gmail.com>
2021-01-19 02:47:37 +0100

    blog preview js and css change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My New Website', body: 'lorem ipsum...', author: 'john', id: 1 },
+    { title: 'Welcome Party', body: 'lorem ipsum...', author: 'Miller', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Trump', id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
