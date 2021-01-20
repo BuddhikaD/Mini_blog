8f64dfe44a06641ecb11e3ea91821e98b211310d
BuddhikaD <buddhiextuse@gmail.com>
2021-01-20 20:03:37 +0100

    make custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,15 @@
 import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My New Website', body: 'lorem ipsum...', author: 'john', id: 1 },
-    { title: 'Welcome Party', body: 'lorem ipsum...', author: 'Miller', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Trump', id: 3 },
-  ]);
-
-  const [name, setName] = useState('John');
-
-  const handleDelete = (id) => {
-    const newBlog = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlog);
-  };
-
-  useEffect(() => {
-    console.log('user effect run');
-    console.log(name);
-  }, [name]);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>Change name</button>
-      <p>{name}</p>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!!" />}
     </div>
   );
 };
