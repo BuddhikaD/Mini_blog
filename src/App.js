80f0012a48759220d819caabee86723331630e3c
BuddhikaD <buddhiextuse@gmail.com>
2021-01-21 01:27:47 +0100

    hook route set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
