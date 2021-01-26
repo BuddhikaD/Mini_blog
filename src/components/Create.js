5380e2081cd4b16f5f3dca76978943a15b8a1e16
BuddhikaD <buddhiextuse@gmail.com>
2021-01-27 00:12:56 +0530

    submit event

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,19 +5,28 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('yoshi');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
   return (
     <div className="create">
       <h2>Add a New Blog!!</h2>
-      <form>
+
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+
         <label> Blog Body:</label>
         <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+
         <label> Blog Author:</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
+
         <button>Add Blog</button>
         <p>{title}</p>
         <p>{body}</p>
